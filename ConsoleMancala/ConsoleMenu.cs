d46257c81aba660c57d91ac8f598fc58ed991e03
Khorsash <yellow-yoda@yandex.ru>
2025-04-19 00:04:03 +0200

    initial commit

---
+++ ConsoleMancala/ConsoleMenu.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleMenu
+{
+    class Menu
+    {
+        public static T[][] Paginate<T>(IEnumerable<T> array, int pageSize)
+        {
+            if (pageSize <= 0)
+            {throw new ArgumentException("Page size must be greater than 0");}
+            if (array == null)
+            {throw new ArgumentNullException("Array is null");}
+
+            T[] list = array.ToArray();
+            int count = list.Length;
+
+            if (count == 0)
+            {throw new ArgumentException("Array is empty");}
+
+            int pageCount = count % pageSize != 0 ?((count - count % pageSize) / pageSize) + 1 : count / pageSize;
+            T[][] pages = new T[pageCount][];
+            for (int i=0; i<pageCount; i++)
+            {
+                pages[i] = new T[pageSize];
+                int start = i * pageSize;
+                int end = start + pageSize < count ? start + pageSize : count;
+                int j = 0;
+                for (int k=start; k<end; k++)
+                {
+                    pages[i][j] = list[k];
+                    j++;
+                }
+            }
+            return pages;
+        }
+        private static void BlockConsole()
+        {
+#pragma warning disable CA1416 // Validate platform compatibility
+            Console.BufferHeight = Console.WindowHeight;
+#pragma warning restore CA1416 // Validate platform compatibility
+#pragma warning disable CA1416 // Validate platform compatibility
+            Console.BufferWidth = Console.WindowWidth;
+#pragma warning restore CA1416 // Validate platform compatibility
+            Console.SetCursorPosition(0, 0);
+        }
+        private static void ShowPage<T>(T[] page, string title = "", ConsoleColor selectionColor = ConsoleColor.Green)
+        {
+            Console.WriteLine("\x1b[3J");
+            Console.Clear();
+            bool cond = false;
+            int YOffset = title != "" ? title.Split("\n").Length : 0;
+            if (title != "") Console.WriteLine(title);
+            for (int i=0; i<page.Length; i++)
+            {
+                if (cond)
+                {
+                    Console.ForegroundColor = default;
+                    cond = false;
+                }
+                if (i == 0)
+                {
+                    Console.ForegroundColor = selectionColor;
+                    cond = true;
+                    Console.WriteLine("> " + Convert.ToString(page[i]));
+                    continue;
+                }
+                Console.WriteLine("  " + Convert.ToString(page[i]));
+            }
+            Console.ForegroundColor = default;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            Console.SetCursorPosition(Convert.ToString(page[0]).Length + 2, YOffset);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+        public static T MenuShow<T>(T[][] pages, int pageIndex = 0, string title = "", ConsoleColor selectionColor = ConsoleColor.Green)
+        {
+            BlockConsole();
+            int choice = 0;
+            
+            int YOffset = title != "" ? title.Split("\n").Length : 0;
+
+            int pi = pageIndex;
+            ShowPage(pages[pi], title, selectionColor);
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(0, choice + YOffset);
+                        Console.ForegroundColor = default;
+                        Console.Write("  " + pages[pi][choice]);
+                        if (choice > 0)
+                        {choice--;}
+                        else
+                        {choice = pages[pi].Length - 1;}
+                        Console.SetCursorPosition(0, choice + YOffset);
+                        Console.ForegroundColor = selectionColor;
+                        Console.Write("> " + pages[pi][choice]);
+                        Console.ForegroundColor = default;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.SetCursorPosition(0, choice + YOffset);
+                        Console.ForegroundColor = default;
+                        Console.Write("  " + pages[pi][choice]);
+                        if (choice < pages[pi].Length - 1)
+                        {choice++;}
+                        else
+                        {choice = 0;}
+                        Console.SetCursorPosition(0, choice + YOffset);
+                        Console.ForegroundColor = selectionColor;
+                        Console.Write("> " + pages[pi][choice]);
+                        Console.ForegroundColor = default;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.WriteLine("\x1b[3J");
+                        Console.Clear();
+                        return pages[pi][choice];
+                    case ConsoleKey.LeftArrow:
+                        if (pi > 0)
+                        {pi--;}
+                        else
+                        {pi = pages.Length - 1;}
+                        ShowPage(pages[pi], title, selectionColor);
+                        choice = 0;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (pi < pages.Length - 1)
+                        {pi++;}
+                        else
+                        {pi = 0;}
+                        ShowPage(pages[pi], title, selectionColor);
+                        choice = 0;
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("\x1b[3J");
+                        Console.Clear();
+                        T? x = default;
+                        return x == null ? pages[0][0] : x;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleMancala/Program.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using ConsoleMenu;
+
+namespace ConsoleMancala
+{
+    
+    class Program
+    {
+        static void ClearConsole()
+        {
+            Console.WriteLine("\x1b[3J");
+            Console.Clear();
+        }
+        static int TurnEndIndex(int[] board, int pitIndx)
+        {
+            int turn = board[board.Length-1];
+            int startIndx = turn == 1 ? pitIndx : pitIndx + 7;
+            int opponentsBar = turn == 1 ? 13 : 6;
+            int piecesCount = board[startIndx] == 1 ? 1 : board[startIndx]-1;
+            int currentIndex = startIndx;
+            while(piecesCount>0)
+            {
+                currentIndex = (currentIndex+1)%(board.Length-1);
+                if(currentIndex == opponentsBar) continue;
+                piecesCount--;
+            }
+            return currentIndex;
+        }
+        static int[] MakeTurn(int[] board, int pitIndx)
+        {
+            int turn = board[board.Length-1];
+            int startIndx = turn == 1 ? pitIndx : pitIndx + 7;
+
+            if(startIndx > 13 || board.Length != 15 || board[startIndx] == 0) 
+            {return board;}
+
+            int[] finalBoard = board.ToArray();
+            int opponentsBar = turn == 1 ? 13 : 6;
+            int ownBar = turn == 1 ? 6 : 13;
+            int piecesCount = board[startIndx] == 1 ? 1 : board[startIndx]-1;
+            finalBoard[startIndx] = board[startIndx] == 1 ? 0 : 1;
+            int currentIndex = startIndx;
+            while(piecesCount>0)
+            {
+                currentIndex = (currentIndex+1)%(board.Length-1);
+                if(currentIndex == opponentsBar) continue;
+                finalBoard[currentIndex]++;
+                piecesCount--;
+            }
+
+            if((turn == 1 ? (currentIndex<6) : (currentIndex>6&&currentIndex<13)) && finalBoard[currentIndex] == 1)
+            {
+                finalBoard[ownBar] += finalBoard[12-currentIndex] + 1;
+                finalBoard[12-currentIndex] = 0;
+                finalBoard[currentIndex] = 0;
+                Console.WriteLine();
+                Console.WriteLine("Ends in own empty");
+                Console.WriteLine();
+            }
+
+            if((turn == 1 ? (currentIndex>6&&currentIndex<13) : (currentIndex<6)) && finalBoard[currentIndex] % 2 == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Even count");
+                Console.WriteLine();
+                finalBoard[ownBar] += finalBoard[currentIndex];
+                finalBoard[currentIndex] = 0;
+            }
+            
+            if(finalBoard.AsSpan(0, 6).ToArray().Sum() == 0)
+            {
+                finalBoard[6] += finalBoard.AsSpan(7, 6).ToArray().Sum();
+                for(int i=7; i<13; i++) finalBoard[i] = 0;
+            }
+            else 
+            {
+                if(finalBoard.AsSpan(7, 6).ToArray().Sum() == 0)
+                {
+                    finalBoard[13] += finalBoard.AsSpan(0, 6).ToArray().Sum();
+                    for(int i=0; i<6; i++) finalBoard[i] = 0;
+                }
+            }
+            
+            if(currentIndex == ownBar)
+            {return finalBoard;}
+            
+            finalBoard[board.Length-1] = turn == 1 ? 2 : 1;
+
+            return finalBoard;
+        }
+
+        // that's actually what grok made ok(relatively all another expirience)
+        // shitty ascii art, lmao
+
+        // * |‾‾| [  ] [  ] [  ] [  ] [  ] [  ] |‾‾|
+        // * |00| ----------------------------- |00|
+        // * |__| [  ] [  ] [  ] [  ] [  ] [  ] |__|
+
+        static string IntTo2DigitString(int num)
+        {
+            return num<10 ? "0"+Convert.ToString(num) : Convert.ToString(num%100);
+        }
+
+        static void ShowBoard(int[] board, int showFromPerspective=0, int selected=-1)
+        {
+            int pitsCount = (board.Length-3)/2;
+            int turn = showFromPerspective != 0 ? showFromPerspective : board[board.Length-1];
+            int turnEnd = selected != -1 ? TurnEndIndex(board, turn == 1 ? selected : selected-7) : -1;
+            ConsoleColor consoleColor = ConsoleColor.DarkGray;
+            Console.Write("|‾‾|");
+            ConsoleColor selectColor = ConsoleColor.Green;
+            ConsoleColor selectColor2 = ConsoleColor.Green;
+            ConsoleColor endIndexColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = consoleColor;
+            if(turn == 1)
+            {
+                for(int i=board.Length-3; i>pitsCount; i--)
+                {
+                    if(selected != -1 && i == turnEnd)
+                    {
+                        // Console.ForegroundColor = endIndexColor;
+                        if(i == selected) Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = endIndexColor;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        // Console.ForegroundColor = endIndexColor;
+                        Console.ForegroundColor = consoleColor;
+                        if(i == selected) Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    if(selected != -1 && i == selected)
+                    {
+                        Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = selectColor2;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    Console.Write(" [");
+                    Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                    Console.Write("]");
+                }
+                Console.Write(" ");
+                
+            }
+            else
+            {
+                for(int i=pitsCount-1; i>-1; i--)
+                {
+                    if(selected != -1 && i == turnEnd)
+                    {
+                        // Console.ForegroundColor = endIndexColor;
+                        if(i == selected) Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = endIndexColor;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        // Console.ForegroundColor = endIndexColor;
+                        Console.ForegroundColor = consoleColor;
+                        if(i == selected) Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    if(selected != -1 && i == selected)
+                    {
+                        Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = selectColor2;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    Console.Write(" [");
+                    Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                    Console.Write("]");
+                }
+                Console.Write(" ");
+            }
+            Console.Write("|‾‾|");
+            Console.WriteLine();
+            int opponentsBar = turn == 1 ? board[board.Length-2] : board[pitsCount];
+            Console.Write("|"+IntTo2DigitString(opponentsBar)+"| ");
+            for(int i=0; i<(pitsCount*5)-1; i++) Console.Write("-");
+            int ownBar = turn == 1 ? board[pitsCount] : board[board.Length-2];
+            if(selected != -1 && (turn == 1 ? 6 : 13) == turnEnd)
+            {
+                Console.Write(" |");
+                Console.ForegroundColor = endIndexColor;
+                Console.Write(IntTo2DigitString(ownBar));
+                Console.ForegroundColor = consoleColor;
+                Console.Write("|");
+            }
+            else
+            {Console.Write(" |"+IntTo2DigitString(ownBar)+"|");}
+            Console.WriteLine();
+            Console.Write("|__|");
+            if(turn == 1)
+            {
+                for(int i=0; i<pitsCount; i++)
+                {
+                    if(selected != -1 && i == turnEnd)
+                    {
+                        // Console.ForegroundColor = endIndexColor;
+                        if(i == selected) Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = endIndexColor;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        // Console.ForegroundColor = endIndexColor;
+                        Console.ForegroundColor = consoleColor;
+                        if(i == selected) Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    if(selected != -1 && i == selected)
+                    {
+                        Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = selectColor2;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    Console.Write(" [");
+                    Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                    Console.Write("]");
+                }
+                Console.Write(" ");
+            }
+            else
+            {
+                for(int i=pitsCount+1; i<board.Length-2; i++)
+                {
+                    if(selected != -1 && i == turnEnd)
+                    {
+                        // Console.ForegroundColor = endIndexColor;
+                        if(i == selected) Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = endIndexColor;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        // Console.ForegroundColor = endIndexColor;
+                        Console.ForegroundColor = consoleColor;
+                        if(i == selected) Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    if(selected != -1 && i == selected)
+                    {
+                        Console.ForegroundColor = selectColor; 
+                        Console.Write(" [");
+                        Console.ForegroundColor = selectColor2;
+                        Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                        Console.ForegroundColor = selectColor;
+                        Console.Write("]");
+                        Console.ForegroundColor = consoleColor;
+                        continue;
+                    }
+                    Console.Write(" [");
+                    Console.Write(board[i]>0?IntTo2DigitString(board[i]):"  ");
+                    Console.Write("]");
+                }
+                Console.Write(" ");
+            }
+            Console.Write("|__|");
+            Console.WriteLine();
+
+            // uncomment to debug 
+            Console.Write("{ ");
+            for(int i=0; i<board.Length; i++) Console.Write(Convert.ToString(board[i])+", ");
+            Console.Write(" }");
+            Console.WriteLine();
+            Console.Write("selected: ");
+            Console.WriteLine(selected);
+            Console.Write("choice index given to function: ");
+            Console.WriteLine(turn == 1 ? selected : selected-7);
+            Console.Write("endIndex: ");
+            Console.WriteLine(turnEnd);
+        }
+
+        static void HotSitGame() 
+        {
+            int[] board = new int[15] {4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0, 1};
+            List<int[]> history = new List<int[]>();
+            history.Add(board);
+            List<int> movesHistory = new List<int>();
+            bool gameRunning = true;
+            bool keyNotSelected;
+            int turn;
+            int i;
+            int winner = 1;
+            while(gameRunning)
+            {
+                turn = board[board.Length-1];
+                ClearConsole();
+                Console.WriteLine("Now's player "+Convert.ToString(turn)+" turn");
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                ClearConsole();
+                ConsoleKeyInfo keyInfo;
+                keyNotSelected = true;
+                i = turn == 1 ? 3 : 10;
+                ShowBoard(board, 0, i);
+                while(keyNotSelected)
+                {
+                    keyInfo = Console.ReadKey();
+                    switch(keyInfo.Key)
+                    {
+                        case ConsoleKey.RightArrow: case ConsoleKey.D:
+                            ClearConsole();
+                            i = turn == 1 ? (i == 5 ? 0 : i+1) : (i == 12 ? 7 : i+1);
+                            ShowBoard(board, 0, i);
+                            break;
+                        case ConsoleKey.LeftArrow: case ConsoleKey.A:
+                            ClearConsole();
+                            i = turn == 1 ? (i == 0 ? 5 : i-1) : (i == 7 ? 12 : i-1);
+                            ShowBoard(board, 0, i);
+                            break;
+                        case ConsoleKey.Enter:
+                            if(board[i] != 0) keyNotSelected = false;
+                            break;
+                    }
+                }
+                board = MakeTurn(board, turn == 1 ? i : i-7);
+                history.Add(board);
+                movesHistory.Add(i);
+                if(board.AsSpan(0, 6).ToArray().Sum()+board.AsSpan(7, 6).ToArray().Sum() == 0)
+                {
+                    winner = board[6] > board[13] ? 1 : 2;
+                    gameRunning = false;
+                }
+            }
+            ClearConsole();
+            ShowBoard(board);
+            Console.WriteLine("Player "+Convert.ToString(winner)+" won!\nSwitch between moves with Right and Left arrow keys\nPress Escape to exit to menu");
+            ConsoleKeyInfo keyInfo2;
+            bool analyzingParty = true;
+            int boardStateIndex = history.Count-1;
+            while(analyzingParty)
+            {
+                keyInfo2 = Console.ReadKey();
+                switch(keyInfo2.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if(boardStateIndex == history.Count-2)
+                        {
+                            boardStateIndex = history.Count-1;
+                            ClearConsole();
+                            ShowBoard(history[boardStateIndex], 0);
+                            Console.WriteLine("Player "+Convert.ToString(winner)+" won!");
+                            break;
+                        }
+                        if(boardStateIndex == history.Count-1)
+                        {
+                            boardStateIndex = 0;
+                            ClearConsole();
+                            ShowBoard(history[boardStateIndex], 0, movesHistory[boardStateIndex]);
+                            Console.WriteLine("Player "+Convert.ToString(winner)+" won!");
+                            break;
+                        }
+
+                        boardStateIndex++;
+                        ClearConsole();
+                        ShowBoard(history[boardStateIndex], 0, movesHistory[boardStateIndex]);
+                        Console.WriteLine("Player "+Convert.ToString(winner)+" won!");
+
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if(boardStateIndex == 0)
+                        {
+                            boardStateIndex = history.Count-1;
+                            ClearConsole();
+                            ShowBoard(history[boardStateIndex], 0);
+                            Console.WriteLine("Player "+Convert.ToString(winner)+" won!");
+                            break;
+                        }
+
+                        boardStateIndex--;
+                        ClearConsole();
+                        ShowBoard(history[boardStateIndex], 0, movesHistory[boardStateIndex]);
+                        Console.WriteLine("Player "+Convert.ToString(winner)+" won!");
+
+                        break;
+                    case ConsoleKey.Escape:
+                        ClearConsole();
+                        analyzingParty = false;
+                        break;
+                
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string[] MenuOptions = new string[4] {"New Hot seat game", "New online game", "Settings", "Exit"};
+            bool Running = true;
+            while(Running)
+            {
+                string choice = Menu.MenuShow(Menu.Paginate(MenuOptions, 2), 0, "", ConsoleColor.Yellow);
+                switch(choice)
+                {
+                    case "New Hot seat game":
+                        HotSitGame();
+                        break;
+                    case "Exit":
+                        Running = false;
+                        ClearConsole();
+                        break;
+                }
+            }
+        }
+    }
+}
