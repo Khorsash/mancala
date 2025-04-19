36d6b590ab388161d925a16f57566176cffa8267
Khorsash <yellow-yoda@yandex.ru>
2025-04-19 23:06:47 +0200

    remove indeed library using 2

--- ConsoleMancala/ConsoleMenu.cs
+++ ConsoleMancala/ConsoleMenu.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
 
 namespace ConsoleMenu
 {
